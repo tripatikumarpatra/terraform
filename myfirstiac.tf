4afa981013ec0652a73135ef065881064faa7ad7
tripatikumarpatra <tripatipatra.etc@gmail.com>
2021-08-29 17:49:14 +0530

    first tf commit

---
+++ myfirstiac.tf
@@ -0,0 +1,29 @@
+variable "accessKey" {
+  default = "AKIA3TP5BCMISIMXHGGP"
+}
+variable "secretKey" {
+  default = "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"
+}
+variable "AWS_REGION" {
+  default = "us-east-2"
+}
+variable "AMI" {
+default = "ami-0b9064170e32bde34"
+}
+
+provider "aws" {
+  access_key = var.accessKey
+  secret_key = var.secretKey
+  region     = var.AWS_REGION
+}
+
+
+resource "aws_instance" "ec2vm" {
+  ami                    = var.AMI
+  instance_type          = "t2.micro"
+  key_name               = "iac"
+  user_data              = file("${path.module}/app1-install.sh")
+  tags = {
+    "Name" = "EC2 Demo"
+  }
+}
